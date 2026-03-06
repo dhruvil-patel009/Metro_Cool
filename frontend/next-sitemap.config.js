e594baf15fa3fa40b1e9d1bdfe42bf42774ae586
Dhruvil Patel <85049831+dhruvil-patel009@users.noreply.github.com>
2026-03-06 11:28:09 +0530

    update a sitemap

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -2,7 +2,45 @@
 module.exports = {
   siteUrl: "https://www.metro-cool.com",
   generateRobotsTxt: true,
+
   changefreq: "daily",
   priority: 0.7,
   sitemapSize: 7000,
-};
+
+  exclude: [
+    "/admin",
+    "/admin/*",
+    "/auth",
+    "/auth/*",
+    "/technician",
+    "/technician/*",
+    "/profile",
+    "/profile/*",
+    "/checkout",
+    "/addresses",
+    "/bookings",
+    "/bookings/*",
+    "/addresses/*",
+  ],
+
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        disallow: [
+          "/admin",
+          "/auth",
+          "/technician",
+          "/profile",
+          "/checkout",
+          "/bookings",
+          "/addresses",
+        ],
+      },
+    ],
+  },
+}
